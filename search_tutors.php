7a9e71484852a105d9abe8aba1539f4017038fc1
bharath@971 <bharathkumarkandimalla@gmail.com>
2019-12-13 12:38:23 -0500

    Update search_tutors.php

--- search_tutors.php
+++ search_tutors.php
@@ -49,7 +49,8 @@ WHERE T.zipcode=A.zipcode AND R.aid=T.aid AND
 T.zipcode IN (SELECT A2.zipcode
 FROM AddressMapping A1, AddressMapping A2
 WHERE A1.zipcode= ".$zipcode." AND A2.city=A1.city AND A2.stateName=A1.stateName)
-AND (Tsub.aid = T.aid AND Tsub.subid >= 0 AND Tsub.subid <= 500)";
+AND (Tsub.aid = T.aid AND Tsub.subid >= 0 AND Tsub.subid <= 500)
+AND T.wage <= ".$wage."";
 
       //$tsql1 = "SELECT TOP (10) * FROM [dbo].[Tutors]";
       //echo $tsql1;
