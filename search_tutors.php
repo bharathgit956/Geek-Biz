92baaab9eaa76db8888f74d42aaac564919192e4
bharath@971 <bharathkumarkandimalla@gmail.com>
2019-12-13 12:50:28 -0500

    Update search_tutors.php

--- search_tutors.php
+++ search_tutors.php
@@ -17,8 +17,8 @@ $input = json_decode(file_get_contents('php://input'),true);
 $result = array();
 $zipcode = $_GET["zip"];
 $rating = 0;
-
-
+$gender1 = 'male';
+$gender2 = 'female';
 try {
   $rating = $_GET["rating"];
 } catch (Error $e) { // this will catch only Errors
@@ -45,7 +45,7 @@ T.zipcode IN (SELECT A2.zipcode
 FROM AddressMapping A1, AddressMapping A2
 WHERE A1.zipcode= ".$zipcode." AND A2.city=A1.city AND A2.stateName=A1.stateName)
 AND (Tsub.aid = T.aid AND Tsub.subid >= 0 AND Tsub.subid <= 500)
-AND (T.gender='female' OR T.gender='male')";
+AND (T.gender= ".$gender1." OR T.gender=".$gender2.")";
 
       //$tsql1 = "SELECT TOP (10) * FROM [dbo].[Tutors]";
       //echo $tsql1;
